515dfbe66fff908183fc22a09e88a005dd785c7e
Egor Vorontsov <sdoregor@sdore.me>
2021-10-04 01:35:04 +0300

    Изменена структура `Module`.

--- modules/__init__.py
+++ modules/__init__.py
@@ -10,12 +10,25 @@ class Module(XABC):
 	bot: Bot
 	events: list = ...
 
-	def __init__(self, bot):
+	def __init__(self, bot, conf, **kwargs):
+		super().__init__(**kwargs)
 		self.bot = bot
+
+	def __repr__(self):
+		return f"<Module {self}>"
+
+	def __str__(self):
+		return self.__class__.__module__.partition('modules.')[2]
 
 	async def init(self): ...
 
-	async def proc(self): ...
+	async def start(self): ...
+
+	async def stop(self): ...
+
+	@classproperty
+	def type(cls):
+		return cls.__module__.partition('modules.')[2].partition('.')[0]
 
 # by Sdore, 2021
 # stbot.sdore.me
