c3ccfb3b0f4be4dc4e0da1b04687f5cc64ce096c
Egor Vorontsov <sdoregor@sdore.me>
2022-09-27 10:28:08 +0300

    Добавлен модуль публичного HTTP API.

---
+++ modules/core/api.py
@@ -0,0 +1,54 @@
+# StankinBot API core module
+
+""" Модуль API
+"""
+
+from __future__ import annotations
+
+import asyncio
+from aiohttp import web
+from . import CoreModule
+from ..utils import *
+
+@export
+class APIModule(CoreModule):
+	events = []
+
+	# public:
+	unix: str
+
+	# private:
+	app: -- web.Application
+
+	# internal:
+	_runner: -- web.AppRunner
+	_site: -- web.UnixSite
+
+	async def init(self):
+		app = self.app = web.Application()
+		app.add_routes((
+			web.get('/schedule', self.handle_schedule),
+		))
+
+	async def start(self):
+		runner = self._runner = web.AppRunner(self.app)
+		await runner.setup()
+		site = self._site = web.UnixSite(runner, self.unix)
+		await site.start()
+
+	async def stop(self):
+		try: self._runner
+		except AttributeError: pass
+		else: await self._runner.cleanup()
+
+	async def handle_schedule(self, request):
+		group = request.query.get('group')
+		if (not group): raise web.HTTPBadRequest(reason="`group' parameter is required.")
+
+		try: schedule = self.bot.modules.backend.schedule.schedules[group.upper()]
+		except KeyError: raise web.HTTPNotFound(reason="No schedule for this group.")
+
+		return web.json_response(schedule.to_json())
+
+# by Sdore, 2022
+# stbot.sdore.me
