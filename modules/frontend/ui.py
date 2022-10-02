caeb0fc9fa0c8cbca384b18603173fe9a051935d
Egor Vorontsov <sdoregor@sdore.me>
2022-10-02 15:06:54 +0300

    Добавлен шаблон модуля UI. WIP egormanga/StankinBot#12

---
+++ modules/frontend/ui.py
@@ -0,0 +1,17 @@
+# StankinBot UI frontend module
+
+""" Модуль взаимодействия с пользователем (UI)
+См. https://github.com/egormanga/StankinBot/issues/12
+"""
+
+from __future__ import annotations
+
+from . import FrontendModule
+
+@export
+class UIModule(FrontendModule):
+	# attributes:
+	events = []
+
+# by Sdore, 2022
+# stbot.sdore.me
